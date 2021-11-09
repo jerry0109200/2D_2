db4f66946a1945cc203fb62d13d2cee5d049a819
jerry0109200 <108051017@live.asia.edu.tw>
2021-11-09 15:35:44 +0800

    11_9
    11/9

--- New Unity Project (1)/Assets/Scripts/RubyMove.cs
+++ New Unity Project (1)/Assets/Scripts/RubyMove.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class RubyMove : MonoBehaviour
 {
 
-    public float moveSpeed = 10;
+    public float moveSpeed;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 rubyMove  = new Vector2();
-        rubyMove = transform.position;
-        rubyMove.x += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        rubyMove.y += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.position = rubyMove;
+        Vector2 rubPosition = transform.position;
+        rubPosition.x = rubPosition.x + moveSpeed * Input.GetAxis("Horizontal");
+        rubPosition.y = rubPosition.y + moveSpeed * Input.GetAxis("Vertical");
+        rb.MovePosition(rubPosition);
     }
 }
